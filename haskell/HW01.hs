9bbb87f90a6f7f81c6abcb63eca048a2170651a8
felipemarley <felipemarley34@gmail.com>
2024-10-14 20:18:16 -0300

    soma

--- haskell/HW01.hs
+++ haskell/HW01.hs
@@ -1,1 +1,113 @@
-data Oi = Oi deriving (Show)
+module HW01 where
+
+import Prelude hiding (Num(..))
+
+data Nat = O | S Nat
+    deriving (Eq, Show)
+
+plus :: (Nat, Nat) -> Nat
+plus (n, O) = n
+plus (n, S m) = S (plus(n, m)) 
+
+--currificado
+(<+>) :: Nat -> Nat -> Nat
+n <+> O = n
+n <+> S m = S (n <+> m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--abreviação 
+o    = O
+so   = S o
+sso  = S so
+ssso = S sso
+sssso = S ssso
+ssssso = S sssso
+sssssso = S ssssso
+ssssssso = S sssssso
+sssssssso = S ssssssso
+ssssssssso = S sssssssso
+sssssssssso = S ssssssssso
+ssssssssssso = S sssssssssso
+sssssssssssso = S ssssssssssso
+ssssssssssssso = S sssssssssssso
+sssssssssssssso = S ssssssssssssso
+ssssssssssssssso = S sssssssssssssso
+sssssssssssssssso = S ssssssssssssssso
+ssssssssssssssssso = S sssssssssssssssso
+sssssssssssssssssso = S ssssssssssssssssso
+ssssssssssssssssssso = S sssssssssssssssssso
+sssssssssssssssssssso = S ssssssssssssssssssso
+ssssssssssssssssssssso = S sssssssssssssssssssso
+sssssssssssssssssssssso = S ssssssssssssssssssssso
+ssssssssssssssssssssssso = S sssssssssssssssssssssso
+sssssssssssssssssssssssso = S ssssssssssssssssssssssso
+ssssssssssssssssssssssssso = S sssssssssssssssssssssssso
+sssssssssssssssssssssssssso = S ssssssssssssssssssssssssso
+ssssssssssssssssssssssssssso = S sssssssssssssssssssssssssso
+sssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssssssssssssso
+ssssssssssssssssssssssssssssssssssssssssssssso = S ssssssssssssssssssssssssssssssssssssssssssssso
+sssssssssssssssssssssssssssssssssssssssssssssso = S sssssssssssssssssssssssssssssssssssssssssssssso
