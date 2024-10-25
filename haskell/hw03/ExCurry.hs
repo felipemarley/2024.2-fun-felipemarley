15ce32bbfb89e352e941113cebd62e58fd606065
felipemarley <felipemarley34@gmail.com>
2024-10-25 19:57:10 -0300

    ExCurry finished (?)

--- haskell/hw03/ExCurry.hs
+++ haskell/hw03/ExCurry.hs
@@ -6,11 +6,23 @@ import Prelude hiding ( curry , uncurry )
 
 -- curry gets a "traditional" binary function
 -- and returns its currified version
-curry :: (a -> b) -> c -> a -> b -> c 
-curry = undefined
+curry :: ((a, b) -> c) -> (a -> b -> c)
+curry f x y = f (x, y)
 
 -- uncurry gets a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (x, y) = f x y 
 
+{-
+addTuple :: (Int, Int) -> Int
+addTuple (x, y) = x + y
+
+addCurried :: Int -> Int -> Int
+addCurried x y = x + y
+
+main :: IO ()
+main = do
+    print (uncurry (curry addTuple) (3, 4)) -- Deve retornar 7
+    print (curry (uncurry addCurried) 3 5)  -- Deve retornar 7
+    -}
